cf5f42baa727834a455892b33cd59576ca2d49d4
Yi Zhang <zhanyi@microsoft.com>
2021-04-19 22:53:32 -0700

    minor fix

--- scripts/make_vs_commits.sh
+++ scripts/make_vs_commits.sh
@@ -2,15 +2,14 @@
 
 set -ex
 
-# In bash 0 is True, 1 is False
 is_in_local () {
     local branch=${1}
     local existed_in_local=$(git branch --list ${branch})
 
     if [[ -z ${existed_in_local} ]]; then
-        echo 1
-    else
         echo 0
+    else
+        echo 1
     fi
 }
 
@@ -19,9 +18,9 @@ is_in_remote() {
     local existed_in_remote=$(git ls-remote --heads origin ${branch})
 
     if [[ -z ${existed_in_remote} ]]; then
-        echo 1
-    else
         echo 0
+    else
+        echo 1
     fi
 }
 
@@ -39,11 +38,11 @@ add_vs_commit () {
     git remote set-url origin https://mszhanyi:${pytorch_token}@github.com/mszhanyi/${repo}.git
     git push
 
-    if [[ $(is_in_local "zhanyi/updatevs") ]]; then
+    if [[ "$(is_in_local "zhanyi/updatevs")" == "1" ]] ; then
         git branch -d zhanyi/updatevs
     fi
 
-    if [[ $(is_in_remote "zhanyi/updatevs") ]]; then
+    if [[ "$(is_in_remote "zhanyi/updatevs")" == "1" ]] ; then
         git push origin --delete zhanyi/updatevs
     fi
 

0c6d4757aabedce5d38394c28d59b4998c105b52
Yi Zhang <zhanyi@microsoft.com>
2021-04-27 22:56:56 -0700

    minor fix

--- scripts/updatecuda.py
+++ scripts/updatecuda.py
@@ -3,7 +3,7 @@
 import os
 import lxml.html as lh
 
-from .utils import updatefile
+import utils
 
 def update_build_definition(major_version, minor_version, master_only=True):
     file_name = ".circleci/cimodel/data/windows_build_definitions.py"
@@ -12,7 +12,7 @@ def update_build_definition(major_version, minor_version, master_only=True):
         lines = f.readlines()
         i = 0
         for line in lines:
-            if line.find(f"# VS2019 CUDA-{major_version}"):
+            if line.find(f"# VS2019 CUDA-{major_version}") != -1:
                 indent_str = "    "
                 updates.append((i, f"{indent_str}# VS2019 CUDA-{major_version}.{minor_version}"))
                 updates.append((i+1, f"{indent_str}WindowsJob(None, _VC2019, CudaVersion({major_version}, {minor_version})),"))
@@ -21,11 +21,11 @@ def update_build_definition(major_version, minor_version, master_only=True):
                 updates.append((i+3, f"{indent_str}WindowsJob(2, _VC2019, CudaVersion({major_version}, {minor_version}){master_only_str}),"))
                 break
             i += 1
-    updatefile(file_name, updates)
+    utils.updatefile(file_name, updates)
 
 if __name__ == "__main__":
-    major_version = os.environ.get["CUDA_MAJOR_VERSION"]
-    minor_version = os.environ.get["CUDA_MINOR_VERSION"]
+    major_version = os.environ.get("CUDA_MAJOR_VERSION")
+    minor_version = os.environ.get("CUDA_MINOR_VERSION")
     update_build_definition(major_version, minor_version)
 
 
